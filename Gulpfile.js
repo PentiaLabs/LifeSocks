08183997cd121cbe85a6f1ccecc07fdc00d88989
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-01-08 11:56:25 +0100

    Add simple test task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
+  console.log('We do nothing!');
+});
+
+gulp.task('test', function() {
   console.log('Eh, good luck mates! We need to make some awesome stuff for this gulpfile!');
 });
