de7d2601092cfa6358533dedd4e77e3fdb173f7d
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-01-14 08:48:35 +0100

    Added more gulp tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,31 @@
 var gulp = require('gulp');
+var browserSync = require("browser-sync");
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  console.log('We do nothing!');
+// use default task to launch BrowserSync and watch JS files
+gulp.task('default', ['browser-sync'], function () {
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("client/scripts/*.js", ['js', browserSync.reload]);
+    gulp.watch("client/styles/*.scss", ['sass']);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./client/"
+        }
+    });
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(reload({stream:true}));
 });
 
 gulp.task('test', function() {
