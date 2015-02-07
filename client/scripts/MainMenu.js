a34e23df810ae4dcd9eb7c18ad4ca8eae582800a
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-02-07 20:35:46 +0100

    More states

--- client/scripts/MainMenu.js
+++ client/scripts/MainMenu.js
@@ -1,11 +1,7 @@
 LifeSocks.MainMenu = function(game) {};
 LifeSocks.MainMenu.prototype = {
 	create: function() {
-		this.startButton = this.add.button((320-146)/2, 200, 'button-start', this.startGame, this, 1, 0, 2);
-		instructions = this.game.add.text(
-			60, 250, "Use arrow keys on desktop, \n  accelerometer on mobile",
-			{ font: "16px Arial", fill: "#b921fe", stroke: "#22053a", strokeThickness: 3 }
-		);
+		this.startButton = this.add.button(400, 400, 'button-start', this.startGame, this, 2, 1, 0);
 	},
 	startGame: function() {
 		this.game.state.start('Game');
--- client/scripts/game.js
+++ client/scripts/game.js
@@ -1,26 +1,9 @@
 // Move game logic here...
 LifeSocks.Game = function(game) {
-    keys = null;
-    ball = null;
-    walls = null;
-    timer = 0;
-    totalTimer = 0;
-    loop = null;
-    firstRun = true;
-    level = 0;
-    sfx_bounce = null;
-    maxLevels = 5;
-    audio = true;
+    
 };
 LifeSocks.Game.prototype = {
     create: function() {
         this.add.sprite(0, 0, 'screen-bg');
-        window.addEventListener("deviceorientation", this.handleOrientation, true);
-    },
-    handleOrientation: function(e) {
-        var x = e.gamma; // range [-90,90]
-        var y = e.beta;  // range [-180,180]
-        ball.body.velocity.x += x/2;
-        ball.body.velocity.y += y;
     }
 };
