ab366fa01e6683ea0d13ddb13ad1668404e978b2
teltploek <brian.frisch@gmail.com>
2015-02-07 12:21:58 +0100

    azure

--- server/server.js
+++ server/server.js
@@ -45,6 +45,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(80, function(){
-	console.log('listening on *:80');
+http.listen(process.env.PORT || 3000, function(){
+	console.log('listening on *:' + process.env.PORT || 3000);
 });
