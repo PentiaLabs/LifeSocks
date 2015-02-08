0b868ff19238bd06d1f52e599e757528ddeabf68
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-02-08 02:36:50 +0100

    Add score state

--- client/scripts/Controller/MainMenu.js
+++ client/scripts/Controller/MainMenu.js
@@ -12,12 +12,23 @@ LifeSocks.MainMenu.prototype = {
 	    //this.add.sprite(1200, 250, 'controller-right');
 
 	    button = this.add.button(1200, 250, 'controller-right', this.actionOnClick, this, 2, 1, 0);
-
+ 		button.onInputOver.add(this.over, this);
+	    button.onInputOut.add(this.out, this);
+	    button.onInputUp.add(this.up, this);
 
 	    //this.game.state.start('Game');
 	},
 	startGame: function() {
 	  this.add.sprite(0, 0, 'screen-bg');
+	},
+	 up: function() {
+	    console.log('button up', arguments);
+	},
+	over: function() {
+	    console.log('button over');
+	},
+	out: function () {
+	    console.log('button out');
 	},
 	actionOnClick: function () {
 	    console.log('VILDT!!!');
