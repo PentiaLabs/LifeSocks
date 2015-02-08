fbbf545a74b151190cb401172aa49c0191cd4dc6
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-02-08 01:05:40 +0100

    Score screen

---
+++ client/scripts/Board/Score.js
@@ -0,0 +1,11 @@
+LifeSocks.Score = function(game) {};
+LifeSocks.Score.prototype = {
+	create: function() {
+	    var text = "Somebody";
+	    var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+
+	    var t = this.add.text(this.world.centerX-300, 0, text, style);
+	    this.add.sprite(0, 0, 'score-bg');
+
+	}
+};
