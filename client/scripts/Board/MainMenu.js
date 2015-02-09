ce1e85b3a9ca3d2aab787cb29ad33537c3583aef
teltploek <brian.frisch@gmail.com>
2015-02-09 16:18:14 +0100

    players now seem to join game on lobby screen - hardcoded for now, and in need of more coordinates

--- client/scripts/Board/MainMenu.js
+++ client/scripts/Board/MainMenu.js
@@ -1,3 +1,21 @@
+// these are the coordinates in which joined players will be placed
+var joinSlots = [
+	{ x: 215,
+		y: 528 },
+	{ x: 404,
+		y: 605 },
+	{ x: 603,
+		y: 587 },
+	{ x: 797,
+		y: 618 },
+	{ x: 975,
+		y: 591 },
+	{ x: 1158,
+		y: 630 },
+	{ x: 1326,
+		y: 565 }
+];
+
 LifeSocks.MainMenu = function(game) {
 	var that = this;
 
@@ -9,15 +27,30 @@ LifeSocks.MainMenu = function(game) {
 LifeSocks.MainMenu.prototype = {
 	create: function() {
 
-	    var text = "Life Socks";
-	    var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+    // var text = "Life Socks";
+    // var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
 
-	    var t = this.add.text(this.world.centerX-300, 0, text, style);
-	    this.add.sprite(0, 0, 'screen-bg');
+    // var t = this.add.text(this.world.centerX-300, 0, text, style);
+    this.add.sprite(0, 0, 'screen-bg');
+
+    var game = this;
+
+    // temporarily add joined players (hardcoded for now)
+    for (var i = 0; i < joinSlots.length; i++) {
+	    setTimeout(function () {
+	    	game.playerJoined();
+	  	}, (i+1) * 1000);
+	  }
 
 		board.emit('boardReadyToPlay');
-
 	},
+
+	playerJoined: function () {
+		var joinSlot = joinSlots.shift();
+
+		var joinedPlayer = this.add.sprite(joinSlot.x, joinSlot.y, 'semen', 'ready1');
+	},
+
 	startGame: function() {
 		this.game.state.start('Game');
 	}
