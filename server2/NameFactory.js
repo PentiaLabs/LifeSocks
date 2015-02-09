867a3076d26a6e148ec7a277675c24fb05c69c9d
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-02-09 20:18:08 +0100

    Added WIP Server2
    The new version of the server, work in progress

---
+++ server2/NameFactory.js
@@ -0,0 +1,30 @@
+var NameFactory = function() {
+	var names = [
+		'Arthur',
+		'Bone',
+		'Cherry',
+		'Doc',
+		'Edelweiss',
+		'Fanny',
+		'Ginger',
+		'Herpy',
+		'Iggy',
+		'Joker',
+		'King',
+		'Lucky',
+		'Misty',
+		'Nipple',
+		'Odin',
+		'Pinkie',
+		'Ruby',
+		'Snails',
+		'Titsdale',
+		'Usher'
+	];
+	
+	this.generate = function() {
+		return names[Math.floor(Math.random() * names.length)];
+	}
+}
+
+module.exports = NameFactory;
