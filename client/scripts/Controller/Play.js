6aacfe8e70183192f3ee3017cf155f525b00918b
teltploek <brian.frisch@gmail.com>
2015-02-10 22:58:09 +0100

    corrected text position on controller and added avatar

--- client/scripts/Controller/Play.js
+++ client/scripts/Controller/Play.js
@@ -10,11 +10,18 @@ LifeSocks.Play.prototype = {
 	create: function() {
 		this.add.sprite(0, 0, 'controller-bg');
 
+		var avatar = this.add.sprite(this.world.width / 2, 400, 'semen', 'ready1');
+		avatar.angle = 90;
+		avatar.x = this.world.width / 2 - avatar.width / 2 + avatar.width;
+
 	    var text = LifeSocks.playerData.name;
-	    var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
+	    var style = { font: "65px Arial", fill: "#00000", align: "center" };
 	    var t = this.add.text(this.world.centerX, 600, text, style);
+
+	    console.log(t.width);
+	    t.x = this.world.width / 2 - t.width / 2;
 	    
-	    this.add.button(1200, 250, 'controller-right', this.rotateRight, this, null, null, null);
+	    this.add.button(this.world.width - 528 - 50, 250, 'controller-right', this.rotateRight, this, null, null, null);
 	    this.add.button(50, 250, 'controller-left', this.rotateLeft, this, null, null, null);
 	},
 	restart: function() {
