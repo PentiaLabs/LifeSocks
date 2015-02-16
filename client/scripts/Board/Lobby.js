a2d3dc7c72e8c65de38d8bcee108caf719d6a1e9
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2015-02-16 09:38:23 +0100

    Remove the unused lobby file
    We did not use it, we use the mainmenu file.

--- client/scripts/Board/Lobby.js
+++
@@ -1,9 +0,0 @@
-LifeSocks.Lobby = function() {};
-LifeSocks.Lobby.prototype = {
-	create: function() {
-		this.game.state.start('Game');
-	},
-	startGame: function() {
-		this.game.state.start('Game');
-	}
-};
